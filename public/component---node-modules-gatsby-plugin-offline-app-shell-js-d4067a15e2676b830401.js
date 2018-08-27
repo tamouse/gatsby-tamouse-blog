19a05c8aa8f275b1f841805febb05d75c4df8334
Tamara Temple <tamouse@gmail.com>
2018-08-27 00:08:16 -0500

    adding public (forced) for debugging

---
+++ public/component---node-modules-gatsby-plugin-offline-app-shell-js-d4067a15e2676b830401.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{229:function(e,n,t){"use strict";var u=t(26);n.__esModule=!0,n.default=void 0;var o=u(t(16)),r=u(t(0)),a=function(e){function n(){return e.apply(this,arguments)||this}return(0,o.default)(n,e),n.prototype.render=function(){return r.default.createElement("span",null)},n}(r.default.Component);n.default=a}}]);
+//# sourceMappingURL=component---node-modules-gatsby-plugin-offline-app-shell-js-d4067a15e2676b830401.js.map
